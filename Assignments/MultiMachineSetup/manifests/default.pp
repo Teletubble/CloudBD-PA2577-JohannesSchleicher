a4f70d23a2ccc0fc22e84bb6796063cf5df871c9
s363876 <johannes.schleicher1998@gmail.com>
2021-11-17 11:00:51 +0100

    all files

---
+++ Assignments/MultiMachineSetup/manifests/default.pp
@@ -0,0 +1,50 @@
+node appserver {
+  exec { 'update':
+    path => ['/usr/bin', '/usr/sbin', '/bin'],
+    command => "apt-get update",
+  }
+
+  package { curl:
+    ensure => "installed",
+    require => Exec['update'],
+  }
+
+  exec {'before installing node.js':
+    path => ['/usr/bin', '/usr/sbin', '/bin'],
+    command => "curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -",
+  }
+
+  package { nodejs:
+    ensure => "installed",
+    require => [ 
+    Exec['update'],
+	Exec['before installing node.js']
+   ],
+  }
+}
+
+node dbserver {
+  include mysql
+}
+
+node web {
+  package { 'nginx':
+    ensure => present,
+  }
+
+  service { 'nginx':
+    ensure => running,
+    require => Package['nginx'],
+ }
+}
+
+
+node /tst[0-2]/ {
+  exec { 'update':
+    path => ['/usr/bin', '/usr/sbin', '/bin'],
+    command => "apt-get update",
+  }
+}
+
+node default {
+}
---
+++ Assignments/MultiMachineSetup/modules/mysql/manifests/init.pp
@@ -0,0 +1,21 @@
+# == Class: mysql
+#
+# Installs MySQL server, sets config file, and loads database for dynamic site.
+#
+class mysql {
+  package { ['mysql-server']:
+    ensure => present;
+  }
+
+  service { 'mysql':
+    ensure  => running,
+    require => Package['mysql-server'];
+  }
+
+  exec { 'set-mysql-password':
+    unless  => 'mysqladmin -uroot -proot status',
+    command => "mysqladmin -uroot password root",
+    path    => ['/usr/bin', '/usr/sbin', '/bin'],
+    require => Service['mysql'];
+  }
+}
